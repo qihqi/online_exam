6bbc0189d66af4509d0e7586b3b5dcde2b7c6c38
Han Qi <qihan.dev@gmail.com>
2020-04-23 20:50:47 -0700

    add view submission

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import jinja2
 from jinja2 import Environment, FileSystemLoader
 import bottle
-from bottle import request
+from bottle import request, response
 import sqlalchemy
 
 import models
@@ -42,6 +42,7 @@ def index():
 
 @bottle.get('/user/<uid>/prob')
 def get_prob_page(uid):
+    msg = request.query.get('msg', '')
     with session_scope() as session:
         user = session.query(models.User).filter_by(access_uuid=uid).first()
         if user is None:
@@ -51,23 +52,32 @@ def get_prob_page(uid):
             """Prove that \( x^2 - y^2 = (x - y)(x + y) \)."""
         ]
         return jinja_env.get_template('problems.html').render(
-                user=user, problems=problems)
+                user=user, problems=problems, msg=msg)
 
 
-
-@bottle.post('/upload_solution')
-def recv_solution():
+@bottle.post('/upload_solution/<uid>')
+def recv_solution(uid):
     prob_id = request.forms.get('prob_id')
     link = request.forms.get('link')
     user_id = request.forms.get('user_id')
+    redirect_url = '/user/{}/prob?msg=success'.format(uid)
     sub = models.Submission()
     sub.link = link
     sub.user_id = user_id
     sub.prob_id = prob_id
     with session_scope() as session:
         session.add(sub)
-    return {'status': 'success'}
+    return bottle.redirect(redirect_url)
 
+
+@bottle.get('/all_solutions')
+def all_solutions():
+    with session_scope() as session:
+        submissions = session.query(models.Submission, models.User).filter(
+                models.Submission.user_id == models.User.uid)
+        print(list(submissions))
+        return jinja_env.get_template('submissions.html'
+            ).render(submissions=submissions)
 
 
 if __name__ == '__main__':
